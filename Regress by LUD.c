745b775841da28a41f290b6b3ca9e35630097080
JrPhy <j0958322080@gmail.com>
2019-11-12 13:10:51 +0800

    Update Regress by LUD.c

--- Regress by LUD.c
+++ Regress by LUD.c
@@ -8,16 +8,16 @@
 int main() {
 	int i, j, k, f;	//for loop
 	int m = 504, n = 15;	//for matrix
-	int row, col, change_row[n], change_col[n];
+	int row, col, change_row[n] = {0}, change_col[n] = {0}, max[n] = {0}, maximum; // for find max
 
     //Loading files
 	FILE *fp;
-	float x[m], y[m], cd[m];
-	double A[m][n], B[n][n];
-	double L[n][n], temp, max[n], d[n], maximum, c[n], z[n], Binv[n][n], y_regres[m];
-	fp = fopen("data.txt","r");
+	float x[m], y[m], cd[m], A[m][n]; //for data
+	double B[n][n] = {0}, Binv[n][n], L[n][n]; //inverse
+	double temp = 0, d[n] = {0}, c[n] = {0}, z[n] = {0}, y_regres[m]; // for linear system
+	fp = fopen("data.csv","r");
 		if (fp == NULL) printf("data.txt does not exist\n");
-		for (i=0; i<m; i++) fscanf(fp, "%f	%f	%f\n",&x[i] ,&y[i] ,&cd[i] );
+		for (i=0; i<m; i++) fscanf(fp, "%f,%f,%f\n",&x[i] ,&y[i] ,&cd[i] );
 	fclose(fp);
     //Loading files
 
@@ -39,39 +39,20 @@ int main() {
 		A[i][12] = x[i]*x[i]*y[i]*y[i];
 		A[i][13] = x[i]*y[i]*y[i]*y[i];
 		A[i][14] = y[i]*y[i]*y[i]*y[i];
+		if(i < n) L[i][i] = 1.;
 	}
-	
-    //Initialize
-    for(i=0; i<n; i++)
-	{
-		max[i] = 0.;
-		d[i] = 0.;
-		L[i][i] = 1.;
-		for (j=i+1; j<n; j++)
-		{
-			L[i][j] = 0.;
-			L[j][i] = 0.;
-		}
-		change_row[i] = 0;
-		change_col[i] = 0;
-		c[i] = 0.;
-		z[i] = 0.;
-		for(k=0; k<n; k++) B[j][k] = 0;
-	}
-    //Initialize
-    
+
     //Getting matrix B = A^T*A
 	for(j=0; j<n; j++) for(i=0; i<m; i++) for(k=0; k<n; k++) B[j][k] = B[j][k] + A[i][j]*A[i][k];
     //Getting matrix B = A^T*A
 
- //Getting matrix B^-1
- 
+	//Getting matrix B^-1
+
 	//find the maximum value before (k-1)th times elimination of whole matrix
 	for (k=0; k<n-1; k++)
 	{
 		for (f=k; f<n; f++)
 		{
-			max[f] = 0;
 			for (j=0; j<n; j++)
 			{
 				if(fabs(B[f][j]) > fabs(max[f])) max[f] = B[f][j];
@@ -84,7 +65,7 @@ int main() {
 			}
 		}
 	//find the maximum value before (k-1)th times elimination of whole matrix
-	
+
 	//Choosing the pivot
 		if(fabs(max[k]) < fabs(B[row][col]))
 		{
@@ -102,7 +83,7 @@ int main() {
 			}
 		}
 	//Choosing the pivot
-	
+
 	//Find L (from I) and U (from B) matrix
 		for (i=k+1; i<n; i++)
 		{
@@ -110,10 +91,7 @@ int main() {
 			L[i][col] = d[i];
 			for (j=0; j<n; j++)
 			{
-				if (d[i] != 0)
-				{
-					B[i][j] = B[i][j] - B[k][j]*d[i];
-				}
+				if (d[i] != 0) B[i][j] = B[i][j] - B[k][j]*d[i];
 				else
 				{
 					temp = L[k][j];
@@ -127,10 +105,7 @@ int main() {
 		}
 		for (i=k+1; i<n; i++)
 		{
-			if(B[i][col] != 0)
-			{
-				B[i][col] = B[i][col] - B[k][col]*B[i][col]/maximum;
-			}
+			if(B[i][col] != 0) B[i][col] = B[i][col] - B[k][col]*B[i][col]/maximum;
 		}
 		maximum = 0;
 	}
@@ -167,33 +142,23 @@ int main() {
 		for(k=j+1; k<n; k++)
 		{
 			temp = 0.;
-			for (i=0; i<k; i++)
-			{
-				temp = temp - L[k][i]*z[i];
-			}
+			for (i=0; i<k; i++) temp = temp - L[k][i]*z[i];
 			z[k] = temp;
 		}
 	//Forward substitute
 
 	//Backward substitute
-		for (i=0; i<n; i++)
-		{
-			max[i] = 0;
-		}
+		for (i=0; i<n; i++) max[i] = 0;
 		c[n] = z[n]/B[n][n];
 		for(k=n-1; k>-1; k--)
 		{
 			temp = 0.;
-			for (i=k+1; i<n; i++)
-			{
-				temp = temp - B[k][i]*c[i];
-			}
+			for (i=k+1; i<n; i++) temp = temp - B[k][i]*c[i];
 			c[k] = (z[k] + temp)/B[k][k];
 		}
 		for (i=0; i<n; i++) z[i] = 0;
 	//Backward substitute
-
-		for (i=0; i<n; i++) Binv[i][j] = c[i];
+	for (i=0; i<n; i++) Binv[i][j] = c[i];
 	}
 
 	//permute
